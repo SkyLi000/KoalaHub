c5aeec1ba8cc74cba84a0d7007d62e0667aaf569
SkyLi000 <97918490+SkyLi000@users.noreply.github.com>
2022-04-23 11:48:41 +1200

    Update MerchantHop.lua

--- MerchantHop.lua
+++ MerchantHop.lua
@@ -1,83 +1,27 @@
-local HttpService = game:GetService("HttpService")
----@diagnostic disable: undefined-global
-
-if not getgenv().TotalServers then
-    getgenv().TotalServers = 0
-    getgenv.TotalServersMerchant = 0
-    getgenv().ItemsBought = 0
+if not getgenv().totalServers then
+	getgenv().totalServers = 0
+    getgenv().totalServersMerchant = 0
+    getgenv().itemsBought = 0
 end
 
 spawn(function()
-    local PlaceID = game.PlaceId
-    local AllIDs = {}
-    local FoundAnything = ""
-    local ActualHOur = os.date("!*t").hour
-    local Deleted = false
-    function TPReturner()
-        local Site;
-        if FoundAnything == "" then
-            Site = game.HttpService:JSONDecode(game:HttpGet("https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100"))()
-        else
-            Site = game.HttpService:JSONDecode(game:HttpService("https://games.roblox.com/v1/games/" .. PlaceID .. "/servers/Public?sortOrder=Asc&limit=100&cursor="..FoundAnything))()
-        end
-        local ID = ""
-        if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
-			FoundAnything = Site.nextPageCursor
-		end
-        local num = 0;
-        for i,v in pairs(Site.data) do
-            local Possible = true
-            ID = tostring(v.id)
-            if tonumber(v.maxPlayer) > tonumber(v.playing) then
-                for _,Existing in pairs(AllIDs) do
-                    if num ~= 0 then
-                        if ID == tostring(Existing) then
-                            Possible = false
-                        end
-                    else
-                        if tonumber(ActualHOur) ~= tonumber(Existing) then
-                            local delfile = pcall(function()
-                                delfile("KoalaHub/PetSimX_MerchantHop.json")
-                                AllIDs = {}
-                                table.insert(AllIDs, ActualHOur)
-                            end)
-                        end
-                    end
-                    num = num + 1
-                end
-                if Possible==true then
-                    table.insert(AllIDs, ID)
-                    wait()
-                    pcall(function()
-                        writefile("KoalaHub/PetSimX_MerchantHop.json", game:GetService("HttpService"):JSONEncode(AllIDs))
-                        wait()
-                        game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
-                    end)
-                    wait(4)
-                end
-            end
-        end
+	local PlaceID = game.PlaceId
+	local AllIDs = {}
+	local foundAnything = ""
+	local actualHour = os.date("!*t").hour
+	local Deleted = false
+	function Teleport()
+        game:GetService("TeleportService"):Teleport(6284583030, game.Players.LocalPlayer)
     end
-    function Teleport()
-		while wait() do
-			pcall(function()
-				TPReturner()
-				if FoundAnything ~= "" then
-					TPReturner()
-				end
-			end)
-		end
-	end
 end)
+
 
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
-
-local console = loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyLi000/PetSimXScripts/main/Console.lua"))()
+local console = loadstring(game:HttpGet("https://raw.githubusercontent.com/4lve/SynapseXConsole/main/maine.lua"))()
 console.clear()
-console.log("New Server Joined: Loading Script")
-
+console.log('New Server Joined. Waiting For Script To Load')
 local Lib = require(game.ReplicatedStorage:WaitForChild("Framework"):WaitForChild("Library"))
 while not Lib.Loaded do
 	game:GetService("RunService").Heartbeat:Wait()
@@ -91,16 +35,17 @@ function useTeleport()
     console.log('Teleporting To New Server')
     nextTeleport([[
 		getgenv().mode = ]]..mode..[[
-        getgenv().TotalServers = ]]..TotalServers..[[
-        getgenv().TotalServersMerchant = ]]..TotalServersMerchant..[[
-        getgenv().ItemsBought = ]]..ItemsBought..[[
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyLi000/PetSimXScripts/main/MerchantHop.lua"))()
+        getgenv().totalServers = ]]..totalServers..[[
+        getgenv().totalServersMerchant = ]]..totalServersMerchant..[[
+        getgenv().itemsBought = ]]..itemsBought..[[
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/4lve/Roblox/main/MerchantHop.lua"))()
     ]])
     Teleport()
 end
 
+
 console.new()
-console.windowname('Merchant Hop')
+console.windowname('Merchant Hop Created By 4lve#0001')
 console.clear()
 
 console.log("Server Info:")
@@ -119,23 +64,35 @@ console.newline()
 
 if (Lib.Network.Invoke("get merchant items")["Level 3"]) then
     console.formatcolors(" - &aMerchant Found")
-    if (getgenv().mode == 1) then
+	if (getgenv().mode == 1) then
 		notOutOfStock = true
 		while notOutOfStock do
 			notOutOfStock = Lib.Network.Invoke("buy merchant item", 3)
 			if notOutOfStock then
 				console.newline()
 				console.formatcolors(" - &aMerchant Pet Bought")
-				getgenv().ItemsBought = getgenv().ItemsBought + 1
+				getgenv().itemsBought = getgenv().itemsBought + 1
 			end
 		end
-	end
+	else
 
-    getgenv().TotalServers = getgemv().TotalServers + 1
-    getgenv().TotalServersMerchant = getgenv().TotalServersMerchant + 1
+		for i,v in pairs(Lib.Network.Invoke("get merchant items")) do
+			if ((v.petId == "288") and v.petExtra.r) then
+				notOutOfStock = Lib.Network.Invoke("buy merchant item", tonumber(i:split(" ")[2]))
+				if notOutOfStock then
+					console.newline()
+					console.formatcolors(" - &aBought Rainbow Santa Paws")
+					getgenv().itemsBought = getgenv().itemsBought + 1
+				end
+			end
+		end
+
+	end
+    getgenv().totalServers = getgenv().totalServers + 1
+    getgenv().totalServersMerchant = getgenv().totalServersMerchant + 1
     useTeleport()
 else
     console.formatcolors(" - &4Merchant Not Found")
-    getgenv().TotalServers = getgenv().TotalServers + 1
+    getgenv().totalServers = getgenv().totalServers + 1
     useTeleport()
 end
